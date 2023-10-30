47451c48398d2c7558f40421fd455aba01f25b70
LuanHenriqueDLP <146896186+LuanHenriqueDLP@users.noreply.github.com>
2023-10-30 17:09:00 -0300

    Upload projeto finalizado

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hotsite.Models;
+
+namespace Hotsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Interesse cad)
+        {
+            try
+            {
+                DatabaseService dbs = new DatabaseService();
+                dbs.CadastraInteresse(cad);
+                return Json(new{status="OK"});
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("ERRO ENCONTRADO NA ACTION 'Cadastrar(Interesse cad)', NO CONTROLLER 'Home'.", e.Message);
+                return Json(new{status="ERR", mensagem="Houve um problema ao realizar a sua solicitação, por favor, tente mais tarde."});
+            }
+        }
+
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function()
+{
+    $("#formMensagem").submit(function(e) {
+
+        e.preventDefault();
+        Cadastrar();
+    });
+})
+
+function Cadastrar() 
+{
+    let parametros = {
+        Nome: $("#idNome").val(),
+        Email: $("#idEmail").val(),
+        Mensagem: $("#idMensagem").val()
+    };
+
+    $.post("/Home/Cadastrar", parametros)
+
+    .done(function(data)
+    {
+        if (data.status == "OK")
+        {
+            $("#formMensagem").after("<h4>Solicitação realizada com sucesso!</h4>");
+            $("#formMensagem").hide();
+        }
+        else
+        {
+            alert(data.mensagem);
+        }
+    })
+
+    .fail(function(data) 
+    {
+        alert(data.mensagem);
+    })
+};
